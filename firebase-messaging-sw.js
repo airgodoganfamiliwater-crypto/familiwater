87c74888489054d0726f0ffce5238bf462f375c0
Air Godogan FAMILI WATER <airgodoganfamiliwater@gmail.com>
2026-01-24 23:31:39 +0700

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCl13_a4x-BQnWNUjf9JOQX1DKc-HxLBys",
+  authDomain: "klien-39696.firebaseapp.com",
+  projectId: "klien-39696",
+  storageBucket: "klien-39696.appspot.com",
+  messagingSenderId: "434516298894",
+  appId: "1:434516298894:web:fad97694415bd4237acddf"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[FCM] BG message:', payload);
+
+  const notification = payload.notification || {};
+  const title = notification.title || "Nusantara";
+  const body = notification.body || payload.data.body || "Ada pesan baru";
+
+  self.registration.showNotification(title, {
+    body: body,
+    icon: '/ikon-512.png',
+    badge: '/ikon-512.png',
+    image: notification.image || null,
+    data: payload.data || {}
+  });
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "familiwater-v1";
+const urlsToCache = [
+  "/",
+  "/profil.html",
+  "/game.html",
+  "/logofw.png",
+  "/ikon-512.png",
+  "/ikon-192.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("PWA Service Worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
